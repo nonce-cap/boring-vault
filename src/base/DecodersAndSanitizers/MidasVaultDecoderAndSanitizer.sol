221d9084f34eb9cd7ee7cba2f49d7e1f023fcd93
Vaibhav Valecha <vaibhav@ether.fi>
2026-06-10 15:37:30 -0500

    remove License

--- src/base/DecodersAndSanitizers/MidasVaultDecoderAndSanitizer.sol
+++ src/base/DecodersAndSanitizers/MidasVaultDecoderAndSanitizer.sol
@@ -1,7 +1,3 @@
-// SPDX-License-Identifier: SEL-1.0
-// Copyright © 2025 Veda Tech Labs
-// Derived from Boring Vault Software © 2025 Veda Tech Labs (TEST ONLY – NO COMMERCIAL USE)
-// Licensed under Software Evaluation License, Version 1.0
 pragma solidity 0.8.21;
 
 contract MidasVaultDecoderAndSanitizer {
